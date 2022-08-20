ee3140a80a3adcaf54f49619ca8a576cec30736c
jeff truitt <jtruitt@gmail.com>
2022-08-20 01:21:07 -0700

    react router dom part 5 links completed

---
+++ music_library/src/components/ArtistView.jsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+
+export default function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            <h2>The id passed was: {id}</h2>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
